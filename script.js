6a5255ec7cc950d43520ee12aaa7d85fa88232bf
yan mori <ymori24@basecampcodingacademy.org>
2023-09-20 09:03:51 -0500

    done

--- script.js
+++ script.js
@@ -3,61 +3,53 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 var item = document.getElementsByTagName("li");
 
-function inputLength(){
-	return input.value.length;
-} 
-
-function listLength(){
-	return item.length;
+function inputLength() {
+    return input.value.length;
 }
 
 function createListElement() {
-	// implement creates an element "li"
-	// implement makes text from input field the li text
-	// implement adds li to ul
-	// implement Reset text input field
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
 
+    // Add strikethrough functionality when clicking the list item
+    li.addEventListener("click", crossOut);
 
-	//START STRIKETHROUGH
-	// because it's in the function, it only adds it for new items
-	function crossOut() {
-		//implement me
-	}
+    // Add a delete button
+    var deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("X"));
+    deleteButton.addEventListener("click", deleteListItem);
 
-	li.addEventListener("click",crossOut);
-	//END STRIKETHROUGH
+    li.appendChild(deleteButton);
 
+    ul.appendChild(li);
 
-	// START ADD DELETE BUTTON
-
-		//Use the implemented function from below here
-	
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
-	function deleteListItem(){
-		//Implement me
-	}
-	//END ADD CLASS DELETE
+    // Reset the text input field
+    input.value = "";
 }
 
+//START STRIKETHROUGH
+// because it's in the function, it only adds it for new items
+function crossOut(event) {
+    event.target.classList.toggle("done");
+}
 
-function addListAfterClick(){
-	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
-		createListElement();
-	}
+// ADD CLASS DELETE (DISPLAY: NONE)
+function deleteListItem(event) {
+    event.target.parentElement.remove();
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-		createListElement();
-	} 
+    if (inputLength() > 0 && event.which === 13) {
+        createListElement();
+    }
 }
 
-
-enterButton.addEventListener("click",addListAfterClick);
+enterButton.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
